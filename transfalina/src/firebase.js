2d9d572c9151c15e27ce0076f0973a49d1fafe2f
migohe14 <42949066+migohe14@users.noreply.github.com>
2018-10-27 14:23:04 +0200

    Update firebase.js

--- transfalina/src/firebase.js
+++ transfalina/src/firebase.js
@@ -1,12 +1,7 @@
 import Firebase from 'firebase'
 
 const firebaseApp = Firebase.initializeApp({
-    apiKey: "AIzaSyBBmF0zwQLTjli-unDAvNAfbMvSljR4Yro",
-    authDomain: "transfalina-4e46c.firebaseapp.com",
-    databaseURL: "https://transfalina-4e46c.firebaseio.com",
-    projectId: "transfalina-4e46c",
-    storageBucket: "transfalina-4e46c.appspot.com",
-    messagingSenderId: "157022875548"
+
 });
 
 export const db = firebaseApp.database()
